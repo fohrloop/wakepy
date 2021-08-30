5769e0e1ee591680ef14e34d069472fdce7cd247
Michael Kennedy <mikeckennedy@gmail.com>
2021-08-30 10:44:28 -0700

    Keep screen awake was backwards (take 2) (#15)
    * For issue #13, macOS keep screen awake seems backwards.
    * Add longer timeout to keeping screen awake. See issue #13

--- wakepy/_darwin.py
+++ wakepy/_darwin.py
@@ -2,11 +2,13 @@ from subprocess import Popen, PIPE
 
 # See: https://ss64.com/osx/caffeinate.html
 COMMAND = u"caffeinate"
+
 ARGS = [
-    "-d",
+    "-d",  # Prevent the display from sleeping.
     "-u",  # Create an assertion to declare that user is active. If the dis-
     # play is off, this option turns the display on and prevents the
     # display from going into idle sleep.
+    "-t 2592000",  # Set timeout to 1-month (default is 5 seconds for -u option)
 ]
 
 _process = None
@@ -24,9 +26,9 @@ def set_keepawake(keep_screen_awake=False):
     """
     if keep_screen_awake:
         global _process
-        _process = Popen([COMMAND], stdin=PIPE, stdout=PIPE)
-    else:
         _process = Popen([COMMAND] + ARGS, stdin=PIPE, stdout=PIPE)
+    else:
+        _process = Popen([COMMAND], stdin=PIPE, stdout=PIPE)
 
 
 def unset_keepawake():
