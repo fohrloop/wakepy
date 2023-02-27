9cb839855d5d7fc8e7dee105abfece73e49931f9
Niko Pasanen <niko@pasanen.me>
2023-02-27 22:51:53 +0200

    make jeepney dbus implementation the default

--- wakepy/_linux/__init__.py
+++ wakepy/_linux/__init__.py
@@ -1,8 +1,18 @@
+"""This module provides the set_keepawake and unset_keepawake
+functions for linux. It tries, in this order
+
+(1) jeepney (pure python solution)
+(2) dbus-python (python bindings to dbus. Needs)
+(3) systemd (requires sudo!)
+
+Other possibilities: (not implemented)
+(4) dbus-send: https://dbus.freedesktop.org/doc/dbus-send.1.html 
+"""
 from importlib import import_module
 from wakepy.exceptions import NotSupportedError
 
 
-for module in "_dbus", "_systemd":
+for module in "_jeepney_dbus", "_dbus", "_systemd":
     try:
         my_module = import_module(f".{module}", f"wakepy._linux")
         set_keepawake, unset_keepawake = (
@@ -17,3 +27,5 @@ else:
         "wakepy does only support dbus and systemd based solutions "
         "Pull requests welcome: https://github.com/np-8/wakepy"
     )
+
+print(f"Wakepy using: {my_module.METHOD}")
