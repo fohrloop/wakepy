75c91aa5a257692bbabf3f568324942f1bd44f4f
Niko Pasanen <niko@pasanen.me>
2023-04-09 14:27:12 +0300

    move logic from .keepawake module to executor (WIP)

--- wakepy/_methods.py
+++ wakepy/_methods.py
@@ -111,6 +111,33 @@ class KeepAwakeMethodExecutor:
         except KeepAwakeError as exception:
             self.handle_failure(exception, on_failure=on_failure)
 
+    def get_method_names_from_args_for_system(
+        method_win=None | str | list[str],
+        method_linux=None | str | list[str],
+        method_mac=None | str | list[str],
+        system: System | None = None,
+    ) -> list[str]:
+        """Gets the method names from input argumets for a system. In other words,
+        takes the 'method_win', 'method_linux', 'method_mac' input arguments,
+        looks at selected 'system', and returns the method input arguments for
+        the given system.
+        """
+        system = system or CURRENT_SYSTEM
+
+        method_dict = {
+            System.WINDOWS: method_win,
+            System.LINUX: method_linux,
+            System.DARWIN: method_mac,
+        }
+
+        methodnames = method_dict.get(system, [])
+
+        if methodnames is None:
+            methodnames = get_default_method_names_for_system(system)
+        elif isinstance(methodnames, str):
+            methodnames = [methodnames]
+        return methodnames
+
 
 @dataclass(kw_only=True)
 class KeepawakeMethod:
--- wakepy/keepawake.py
+++ wakepy/keepawake.py
@@ -14,37 +14,7 @@ from __future__ import annotations
 from contextlib import contextmanager
 
 
-from ._methods import (
-    OnFailureStrategyName,
-    System,
-    CURRENT_SYSTEM,
-    SUPPORTED_SYSTEMS,
-    get_default_method_names_for_system,
-)
-
-
-def get_method_names_from_args_for_system(
-    method_win=None | str | list[str],
-    method_linux=None | str | list[str],
-    method_mac=None | str | list[str],
-    system: System | None = None,
-) -> list[str]:
-    system = system or CURRENT_SYSTEM
-
-    if system not in SUPPORTED_SYSTEMS:
-        return []
-
-    methodnames = {
-        System.WINDOWS: method_win,
-        System.LINUX: method_linux,
-        System.DARWIN: method_mac,
-    }.get(system)
-
-    if methodnames is None:
-        methodnames = get_default_method_names_for_system(system)
-    elif isinstance(methodnames, str):
-        methodnames = [methodnames]
-    return methodnames
+from ._methods import OnFailureStrategyName, CURRENT_SYSTEM, KeepAwakeMethodExecutor
 
 
 def set_keepawake(
@@ -52,6 +22,7 @@ def set_keepawake(
     method_linux=None | str | list[str],
     method_mac=None | str | list[str],
     on_method_failure: str | OnFailureStrategyName = OnFailureStrategyName.LOGINFO,
+    on_failure: str | OnFailureStrategyName = OnFailureStrategyName.ERROR,
 ):
     """
     Parameters
@@ -59,10 +30,12 @@ def set_keepawake(
     method_win:
         The
     """
-    method_names = get_method_names_from_args_for_system(
+    excecutor = KeepAwakeMethodExecutor(
         method_win=method_win,
         method_linux=method_linux,
         method_mac=method_mac,
+        on_method_failure=on_method_failure,
+        on_failure=on_failure,
     )
 
     for method_name in method_names:
