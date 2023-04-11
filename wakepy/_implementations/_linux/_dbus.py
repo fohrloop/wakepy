2c49147b9a04467cb69255ae84a8f278df8c1486
Niko Pasanen <niko@pasanen.me>
2023-04-11 10:50:03 +0300

    refactor the _linux._dbus implementation
    - Idea: raise exceptions at function call time, not at function import time.

--- wakepy/_implementations/_linux/_dbus.py
+++ wakepy/_implementations/_linux/_dbus.py
@@ -23,55 +23,70 @@ REQUIREMENTS = [
     "jeepney (python package)",
 ]
 
-
-try:
-    from jeepney.wrappers import MessageGenerator, new_method_call
-    from jeepney.io.blocking import open_dbus_connection
-except ImportError as e:
-    raise KeepAwakeError("Could not import jeepney!") from e
-
-
-try:
-    connection = open_dbus_connection(bus="SESSION")
-except KeyError as e:
-    if "DBUS_SESSION_BUS_ADDRESS" in str(e):
-        raise KeepAwakeError(
-            "DBUS_SESSION_BUS_ADDRESS environment variable not set! "
-            "If running in subprocess, make sure to pass the DBUS_SESSION_BUS_ADDRESS "
-            "environment variable."
-        ) from e
-    raise KeepAwakeError(
-        "Could not set dbus connection to session message bus. "
-    ) from e
-
-
+module_initialized = False
 # The cookie holds represents the inhibition request
 # It is given in the response of the inhibit call and
 # used in the uninhibit call
 cookie = None
+# This can create dbus messages. Created only if needed.
+messagegenerator = None
+# This is the connection to the session message bus. Created only if needed.
+connection = None
 
 
-class ScreenSaveMessageGenerator(MessageGenerator):
-    r"""DBus interface to org.freedesktop.Screensaver"""
-    interface = SCREENSAVER_INTERFACE
+def init_module():
+    """initializes this module. The 3rd party imports must be made when we need
+    them, not when we import this module. as we want any exceptions occur only
+    when the keepawake functions like `set_keepawake` are used, not when
+    they're imported.
 
-    def __init__(
-        self,
-        object_path=SCREENSAVER_OBJECT_PATH,
-        bus_name=SCREENSAVER_BUS_NAME,
-    ):
-        super().__init__(object_path=object_path, bus_name=bus_name)
+    We also need to subclass the imported MessageGenerator, create instance
+    of it and create a connection to the session messsage bus.
+    """
 
-    def inhibit(self, application_name, reason_for_inhibit):
-        return new_method_call(
-            self, "Inhibit", "ss", (application_name, reason_for_inhibit)
-        )
+    global connection, messagegenerator, module_initialized
 
-    def uninhibit(self, cookie):
-        return new_method_call(self, "UnInhibit", "u", (cookie,))
+    try:
+        from jeepney.wrappers import MessageGenerator, new_method_call
+        from jeepney.io.blocking import open_dbus_connection
+    except ImportError as e:
+        raise KeepAwakeError("Could not import jeepney!") from e
 
+    class ScreenSaveMessageGenerator(MessageGenerator):
+        r"""DBus interface to org.freedesktop.Screensaver"""
+        interface = SCREENSAVER_INTERFACE
 
-messagegenerator = ScreenSaveMessageGenerator()
+        def __init__(
+            self,
+            object_path=SCREENSAVER_OBJECT_PATH,
+            bus_name=SCREENSAVER_BUS_NAME,
+        ):
+            super().__init__(object_path=object_path, bus_name=bus_name)
+
+        def inhibit(self, application_name, reason_for_inhibit):
+            return new_method_call(
+                self, "Inhibit", "ss", (application_name, reason_for_inhibit)
+            )
+
+        def uninhibit(self, cookie):
+            return new_method_call(self, "UnInhibit", "u", (cookie,))
+
+    messagegenerator = ScreenSaveMessageGenerator()
+
+    try:
+        connection = open_dbus_connection(bus="SESSION")
+    except KeyError as e:
+        if "DBUS_SESSION_BUS_ADDRESS" in str(e):
+            raise KeepAwakeError(
+                "DBUS_SESSION_BUS_ADDRESS environment variable not set! "
+                "If running in subprocess, make sure to pass the DBUS_SESSION_BUS_ADDRESS "
+                "environment variable."
+            ) from e
+        raise KeepAwakeError(
+            "Could not set dbus connection to session message bus. "
+        ) from e
+
+    module_initialized = True
 
 
 def set_keepawake(keep_screen_awake=False):
@@ -85,6 +100,9 @@ def set_keepawake(keep_screen_awake=False):
         Currently unused as the screen will remain active as a byproduct of
         preventing sleep.
     """
+    if not module_initialized:
+        init_module()
+
     global cookie
     msg_inhibit = messagegenerator.inhibit("wakepy", "wakelock active")
     reply = connection.send_and_get_reply(msg_inhibit)
@@ -93,6 +111,6 @@ def set_keepawake(keep_screen_awake=False):
 
 def unset_keepawake():
     if cookie is None:
-        raise KeepAwakeError("You must set keepawake before unsetting!")
+        raise KeepAwakeError("You must set_keepawake before unsetting!")
     msg_uninhibit = messagegenerator.uninhibit(cookie)
     connection.send_and_get_reply(msg_uninhibit)
