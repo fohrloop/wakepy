a5693841914cbe47dff7a46070f3e364881c0416
Niko Pasanen <niko@pasanen.me>
2023-06-04 15:07:15 +0300

    change dev version to 0.7.0
    - Next release will be 0.7.0 instead of 1.0.0

--- wakepy/__init__.py
+++ wakepy/__init__.py
@@ -1,6 +1,6 @@
 from .keepawake import keepawake, set_keepawake, unset_keepawake
 
-__version__ = "1.0.0dev"
+__version__ = "0.7.0dev"
 
 
 __all__ = ["keepawake", "set_keepawake", "unset_keepawake"]
