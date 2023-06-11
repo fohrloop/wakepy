ff02451d2fe237a4ddf817eea023bd7df6b21ab0
Niko Pasanen <niko@pasanen.me>
2023-06-11 19:28:48 +0300

    version to 0.7.0

--- wakepy/__init__.py
+++ wakepy/__init__.py
@@ -1,6 +1,6 @@
 from ._deprecated import keepawake, set_keepawake, unset_keepawake
 
-__version__ = "0.7.0dev"
+__version__ = "0.7.0"
 
 
 __all__ = ["keepawake", "set_keepawake", "unset_keepawake"]
