e30eb61abae49022ddc09c0a124a5ed4e7fbf0c5
Niko Föhr <fohrloop@gmail.com>
2023-10-15 19:43:00 +0300

    add second test for ActivationResult

--- tests/unit/test_core/test_activationresult.py
+++ tests/unit/test_core/test_activationresult.py
@@ -41,3 +41,42 @@ def test_activation_result():
 
     assert ar.active_methods == ["a-successful-method"]
     assert ar.active_methods_string == "a-successful-method"
+
+
+def test_activation_result_three_successful():
+    ar = ActivationResult(switcher)
+
+    ar._results = [
+        MethodUsageResult(
+            status=SuccessStatus.SUCCESS,
+            method_name="1st.successfull.method",
+        ),
+        MethodUsageResult(
+            status=SuccessStatus.FAIL,
+            failure_stage=StageName.REQUIREMENTS,
+            method_name="fail-requirements",
+            message="Missing requirement: Some SW v.1.2.3",
+        ),
+        MethodUsageResult(
+            status=SuccessStatus.SUCCESS,
+            method_name="2nd-successful-method",
+        ),
+        MethodUsageResult(
+            status=SuccessStatus.SUCCESS,
+            method_name="last-successful-method",
+        ),
+    ]
+
+    assert ar.success
+    assert ar.real_success
+    assert not ar.failure
+
+    assert ar.active_methods == [
+        "1st.successfull.method",
+        "2nd-successful-method",
+        "last-successful-method",
+    ]
+    assert (
+        ar.active_methods_string
+        == "1st.successfull.method, 2nd-successful-method & last-successful-method"
+    )
