25177c446c3ed67b0575fdf8134f31ec6e2ed356
Niko Föhr <fohrloop@gmail.com>
2023-11-12 16:17:49 +0200

    remove the _deprecated package
    This was deprecated 2023-06-11 in wakepy 0.7.0
    
    Removed: set_keepawake, unset_keepawake, keepawake
    
    The 0.8.0 release will have the same functionality as 0.7.0
    but with the new API: keep.running and keep.presenting

--- wakepy/__init__.py
+++ wakepy/__init__.py
@@ -1,6 +1,3 @@
-from ._deprecated import keepawake as keepawake
-from ._deprecated import set_keepawake as set_keepawake
-from ._deprecated import unset_keepawake as unset_keepawake
 from .modes import keep as keep
 
 __version__ = "0.8.0dev"
