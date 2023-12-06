6d4ce8ff20f7bd8ea552b0b5e6d4fd6e5a2d49eb
Niko Föhr <fohrloop@gmail.com>
2023-12-06 23:41:00 +0200

    Revert "remove unused type"
    This reverts commit 33247ec2984e2b62a1920015fbc93180323afa41.

--- wakepy/core/mode.py
+++ wakepy/core/mode.py
@@ -9,7 +9,7 @@ from .method import check_priority_order
 
 if typing.TYPE_CHECKING:
     from types import TracebackType
-    from typing import Optional, Type
+    from typing import List, Optional, Type
 
     from .dbus import DbusAdapter, DbusAdapterTypeSeq
     from .method import Method, PriorityOrder
