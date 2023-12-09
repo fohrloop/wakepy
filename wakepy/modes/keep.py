13ac4c90059f5c67f437044cdd0539dadd2a6970
Niko Föhr <fohrloop@gmail.com>
2023-12-09 22:24:44 +0200

    modify keep.running and keep.presenting
    * Start using the create_mode function in keep.running and
      keep.presenting
    * Add `skip` and `use_only` arguments in keep.running and
      keep.presenting

--- wakepy/modes/keep.py
+++ wakepy/modes/keep.py
@@ -2,15 +2,16 @@ from __future__ import annotations
 
 import typing
 
-from ..core.mode import Mode
+from ..core.constants import ModeName
+from ..core.mode import create_mode
 from ..methods.gnome import GnomeSessionManagerNoIdle, GnomeSessionManagerNoSuspend
 
 if typing.TYPE_CHECKING:
-    from typing import List, Type
+    from typing import List, Optional, Type
 
     from ..core.dbus import DbusAdapter, DbusAdapterTypeSeq
-    from ..core.method import Method
-
+    from ..core.method import Method, StrCollection
+    from ..core.mode import Mode
 
 running_methods: List[Type[Method]] = [
     GnomeSessionManagerNoSuspend,
@@ -20,8 +21,12 @@ presenting_methods: List[Type[Method]] = [
 ]
 
 
-def running(dbus_adapter: Type[DbusAdapter] | DbusAdapterTypeSeq | None = None) -> Mode:
-    """A wakepy mode and context manager for keeping programs running.
+def running(
+    skip: Optional[StrCollection] = None,
+    use_only: Optional[StrCollection] = None,
+    dbus_adapter: Type[DbusAdapter] | DbusAdapterTypeSeq | None = None,
+) -> Mode:
+    """Create a wakepy mode (a context manager) for keeping programs running.
 
     Properties
     ----------
@@ -50,6 +55,16 @@ def running(dbus_adapter: Type[DbusAdapter] | DbusAdapterTypeSeq | None = None) 
 
     Parameters
     ----------
+    skip: list, tuple or set of str or None
+        The names of Methods to remove from the keep.running mode; a
+        "blacklist" filter. Any Method in `skip` but not in the keep.running
+        mode will be silently ignored. Cannot be used same time with
+        `use_only`. Optional.
+    use_only: list, tuple or set of str
+        The names of Methods to select from the keep.running mode; a
+        "whitelist" filter. Means "use these and only these Methods". Any
+        Methods in `use_only` but not in the keep.running mode will raise a
+        ValueError. Cannot be used same time with `skip`. Optional.
     dbus_adapter:
         Optional argument which can be used to define a customer DBus adapter.
 
@@ -59,13 +74,21 @@ def running(dbus_adapter: Type[DbusAdapter] | DbusAdapterTypeSeq | None = None) 
         The context manager for keeping a system running.
 
     """
-    return Mode(methods=running_methods, dbus_adapter=dbus_adapter)
+    return create_mode(
+        modename=ModeName.KEEP_RUNNING,
+        skip=skip,
+        use_only=use_only,
+        dbus_adapter=dbus_adapter,
+    )
 
 
 def presenting(
+    skip: Optional[StrCollection] = None,
+    use_only: Optional[StrCollection] = None,
     dbus_adapter: Type[DbusAdapter] | DbusAdapterTypeSeq | None = None,
 ) -> Mode:
-    """The keep.presenting mode context manager.
+    """Create a wakepy mode (a context manager) for keeping a system running
+    and showing content.
 
     Usage:
 
@@ -79,12 +102,28 @@ def presenting(
 
     Parameters
     ----------
+    skip: list, tuple or set of str or None
+        The names of Methods to remove from the keep.running mode; a
+        "blacklist" filter. Any Method in `skip` but not in the keep.running
+        mode will be silently ignored. Cannot be used same time with
+        `use_only`. Optional.
+    use_only: list, tuple or set of str
+        The names of Methods to select from the keep.running mode; a
+        "whitelist" filter. Means "use these and only these Methods". Any
+        Methods in `use_only` but not in the keep.running mode will raise a
+        ValueError. Cannot be used same time with `skip`. Optional.
     dbus_adapter:
         Optional argument which can be used to define a customer DBus adapter.
+
 
     Returns
     -------
     keep_presenting_mode: Mode
         The context manager for keeping a system presenting content.
     """
-    return Mode(methods=presenting_methods, dbus_adapter=dbus_adapter)
+    return create_mode(
+        modename=ModeName.KEEP_PRESENTING,
+        skip=skip,
+        use_only=use_only,
+        dbus_adapter=dbus_adapter,
+    )
