a261a0674596c06d7d3065f09be842ac54646523
Niko Föhr <fohrloop@gmail.com>
2023-12-10 20:34:21 +0200

    improve keep.running and keep.presenting tests

--- tests/unit/test_modes.py
+++ tests/unit/test_modes.py
@@ -1,6 +1,8 @@
+from unittest.mock import Mock
+
 import pytest
 
-from wakepy.core import Method, ModeName
+from wakepy.core import Method, ModeName, DbusAdapter
 from wakepy.modes import keep
 
 
@@ -23,24 +25,54 @@ def create_methods(monkeypatch, name_prefix: str, modename: ModeName):
     return MethodA, MethodB, MethodC
 
 
-def test_keep_running_mode_creation(monkeypatch):
-    name_prefix = "running"
+@pytest.mark.parametrize(
+    "input_args",
+    [
+        dict(
+            name_prefix="running",
+            function_under_test=keep.running,
+            modename=ModeName.KEEP_RUNNING,
+        ),
+        dict(
+            name_prefix="presenting",
+            function_under_test=keep.presenting,
+            modename=ModeName.KEEP_PRESENTING,
+        ),
+    ],
+)
+def test_keep_running_mode_creation(input_args, monkeypatch):
+    """Simple test for keep.running and keep.presenting. Tests that all input
+    arguments for the functions are passed to the Mode.__init__
+    """
+    name_prefix = input_args["name_prefix"]
+    function_under_test = input_args["function_under_test"]
+
     MethodA, MethodB, MethodC = create_methods(
-        monkeypatch, name_prefix=name_prefix, modename=ModeName.KEEP_RUNNING
+        monkeypatch, name_prefix=name_prefix, modename=input_args["modename"]
     )
 
-    mode = keep.running()
-
+    mode = function_under_test()
     # All the methods for the mode are selected automatically
     assert set(mode.methods) == {MethodA, MethodB, MethodC}
 
     # Case: Test "omit" parameter
-    mode = keep.running(omit=[f"{name_prefix}A"])
+    mode = function_under_test(omit=[f"{name_prefix}A"])
     assert set(mode.methods) == {MethodB, MethodC}
 
     # Case: Test "methods" parameter
-    mode = keep.running(methods=[f"{name_prefix}A", f"{name_prefix}B"])
+    mode = function_under_test(methods=[f"{name_prefix}A", f"{name_prefix}B"])
     assert set(mode.methods) == {MethodB, MethodA}
+
+    # Case: Test "methods_priority" parameter
+    methods_priority = [f"{name_prefix}A", f"{name_prefix}B"]
+    mode = function_under_test(methods_priority=methods_priority)
+    assert mode.methods_priority == methods_priority
+    assert set(mode.methods) == {MethodB, MethodA, MethodC}
+
+    # Case: Test "dbus_adapter" parameter
+    dbus_adapter = Mock(spec_set=DbusAdapter)
+    mode = function_under_test(dbus_adapter=dbus_adapter)
+    assert mode.manager._dbus_adapter == dbus_adapter
 
 
 @pytest.mark.skip("This waits to be fixed")
