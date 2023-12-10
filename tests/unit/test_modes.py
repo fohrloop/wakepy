913e19fa7e848c2d00128d45be76afb7dcf1923b
Niko Föhr <fohrloop@gmail.com>
2023-12-10 19:28:57 +0200

    add tests for keep.running (WIP)

--- tests/unit/test_modes.py
+++ tests/unit/test_modes.py
@@ -1,16 +1,56 @@
 import pytest
 
 from wakepy.modes import keep
+from wakepy.core import ModeName, Method
 
-pytest.skip("These need to be fixed", allow_module_level=True)
+
+def create_methods(monkeypatch, name_prefix: str, modename: ModeName):
+    # empty method registry
+    monkeypatch.setattr("wakepy.core.method.METHOD_REGISTRY", dict())
+
+    class MethodA(Method):
+        name = f"{name_prefix}A"
+        mode = modename
+
+    class MethodB(Method):
+        name = f"{name_prefix}B"
+        mode = modename
+
+    class MethodC(Method):
+        name = f"{name_prefix}C"
+        mode = modename
+
+    return MethodA, MethodB, MethodC
 
 
+def test_keep_running_mode_creation(monkeypatch):
+    name_prefix = "running"
+    MethodA, MethodB, MethodC = create_methods(
+        monkeypatch, name_prefix=name_prefix, modename=ModeName.KEEP_RUNNING
+    )
+
+    mode = keep.running()
+
+    # All the methods for the mode are selected automatically
+    assert set(mode.methods) == {MethodA, MethodB, MethodC}
+
+    # Case: Test "omit" parameter
+    mode = keep.running(omit=[f"{name_prefix}A"])
+    assert set(mode.methods) == {MethodB, MethodC}
+
+    # Case: Test "methods" parameter
+    mode = keep.running(methods=[f"{name_prefix}A", f"{name_prefix}B"])
+    assert set(mode.methods) == {MethodB, MethodA}
+
+
+@pytest.mark.skip("This waits to be fixed")
 def test_keep_running():
     with keep.running() as k:
         assert k.success
         assert not k.failure
 
 
+@pytest.mark.skip("This waits to be fixed")
 def test_keep_presenting():
     with keep.presenting() as k:
         assert k.success
