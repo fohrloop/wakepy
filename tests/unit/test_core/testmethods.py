ce73ee270b461a09d1ef403ed72c2a663755c1be
Niko Föhr <fohrloop@gmail.com>
2023-12-21 15:35:01 +0200

    add iterate_test_methods and two new cases for MethodIs: FAILING and FAILING_MESSAGE

--- tests/unit/test_core/testmethods.py
+++ tests/unit/test_core/testmethods.py
@@ -1,26 +1,52 @@
+from __future__ import annotations
+
 import enum
 import itertools
+import typing
 
 from wakepy.core import CURRENT_PLATFORM
 from wakepy.core.method import Method
 
+if typing.TYPE_CHECKING:
+    from typing import Type, Iterable
+
 
 class MethodIs(enum.IntEnum):
-    MISSING = 0
-    SUCCESSFUL = 1
-    RAISING_EXCEPTION = 2
+    MISSING = 0  # not implemented
+    SUCCESSFUL = 1  # Returns True
+    FAILING = 2  # Returns False
+    FAILING_MESSAGE = 3  # Return a string (counts as fail)
+    RAISING_EXCEPTION = 4
 
 
 def create_test_method_classes():
-    """Helper function for get_test_method_class. (See docs there)"""
+    """Helper function for get_test_method_class. (See docs there)
+    This basically creates a bunch of Method classes with the
+    Method.enter_mode, Method.heartbeat and Method.exit_mode defined
+    differently. We do this to make it easier to test every combination
+    possible. (5*5*5 = 125 combinations, if number of options in MethodIs is 5)
+    """
     test_method_classes = dict()
 
     class TestException(Exception):
         ...
 
+    # When enter_mode(), heartbear() or exit_mode() returns True, it is
+    # considered as being successful
     def successful(self):
-        ...
+        return True
 
+    # When enter_mode(), heartbear() or exit_mode() returns False or a string,
+    # it is considered as failing
+    def failing(self):
+        return False
+
+    def failing_message(self):
+        return "failure_reason"
+
+    # The enter_mode(), heartbeat() or exit_mode() *might* raise an exception,
+    # like any other method or function ever made. Althought, if there's an
+    # exception, it means the implementation has a bug.
     def raising_exception(self):
         raise TestException("foo")
 
@@ -32,6 +58,10 @@ def create_test_method_classes():
         ):
             if mode == MethodIs.SUCCESSFUL:
                 clskwargs[name] = successful
+            elif mode == MethodIs.FAILING:
+                clskwargs[name] = failing
+            elif mode == MethodIs.FAILING_MESSAGE:
+                clskwargs[name] = failing_message
             elif mode == MethodIs.RAISING_EXCEPTION:
                 clskwargs[name] = raising_exception
             else:
@@ -49,18 +79,32 @@ def get_test_method_class(
     enter_mode: MethodIs = MethodIs.MISSING,
     heartbeat: MethodIs = MethodIs.MISSING,
     exit_mode: MethodIs = MethodIs.MISSING,
-):
+) -> Type[Method]:
     """Get a test Method class with the .enter_mode(), .heartbeat() and
     .exit_mode() methods as wanted.
 
-    Each of the methods is either
-    0: not implemented (missing method)
-    1: successful (implemented and not raising exception)
-    2: exception (implemented and raising exception)
+    MISSING: missing method
+    SUCCESSFUL: return True
+    FAILING: return False
+    FAILING_MESSAGE: return "failure_reason" (string, counted as fail)
+    RAISING_EXCEPTION: raises TestException("foo")
 
     For the expected outcome of each of these, see:
-        tests/unit/test_core/test_method.py
+        tests/unit/test_core/test_activation.py
     """
     return _test_method_classes[
         f"M{enter_mode.value}{heartbeat.value}{exit_mode.value}"
     ]
+
+
+def iterate_test_methods(
+    enter_mode=Iterable[MethodIs],
+    heartbeat=Iterable[MethodIs],
+    exit_mode=Iterable[MethodIs],
+) -> Iterable[Method]:
+    for enter_mode_, heartbeat_, exit_mode_ in itertools.product(
+        enter_mode, heartbeat, exit_mode
+    ):
+        yield get_test_method_class(
+            enter_mode=enter_mode_, heartbeat=heartbeat_, exit_mode=exit_mode_
+        )()
