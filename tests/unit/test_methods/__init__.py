69de9b2c5ac725455eed089b32c95262a1883c07
Niko Föhr <fohrloop@gmail.com>
2024-01-04 11:10:04 +0200

    simplify module docstring
    unit/test_methods

--- tests/unit/test_methods/__init__.py
+++ tests/unit/test_methods/__init__.py
@@ -3,17 +3,9 @@ Mode.
 
 Tests include:
 * Test for using all the defined methods; for example, running `enter_mode()`
- and  `exit_mode` using mocks in place of 3rd party software
+ and  `exit_mode` using fake/mock adapters (DBusAdapter, etc.)
 
 Tests Exclude:
-* Calling io; No calling any executables or dbus methods (3rd party sw). First,
-  3rd party sw is assumed to be tested in their respective repos and their
-  functionality will not be tested in the automated tests of wakepy. Although,
-  known bugs in specific versions of 3rd party sw will be acted upon in wakepy,
-  when possible. Second, calling io in wakepy is done through Adapters, and the
-  adapters are tested separately.
-* Testing that the 3rd party sw calls are actually written correctly (as we
-  don't call 3rd party sw). This means that the names defining the SW, i.e. the
-  MyMethod(Method) subclass with its attributes, must be tested *manually*
-  once (and only once).
+* IO when processing Calls
+* Usage of Dbus services, even fake ones
 """
