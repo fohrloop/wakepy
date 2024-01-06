237d3c80c957376f83c36eb2b379ea2ce0874c25
Niko Föhr <fohrloop@gmail.com>
2024-01-06 12:26:09 +0200

    make DbusAdapter usable from wakepy root

--- wakepy/__init__.py
+++ wakepy/__init__.py
@@ -1,5 +1,6 @@
 # NOTE The methods sub-package is imported for registering all the methods.
 from . import methods as methods
+from .core import DbusAdapter as DbusAdapter
 from .core import Method as Method
 from .core import Mode as Mode
 from .modes import keep as keep
