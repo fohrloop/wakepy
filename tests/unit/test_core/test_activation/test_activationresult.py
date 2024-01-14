a06a8f4377280f07dc2e11f08650280a80c19b3b
Niko Föhr <fohrloop@gmail.com>
2024-01-14 16:09:29 +0200

    fix broken tests

--- tests/unit/test_core/test_activation/test_activationresult.py
+++ tests/unit/test_core/test_activation/test_activationresult.py
@@ -2,7 +2,7 @@ import re
 import pytest
 
 from wakepy.core import ActivationResult, MethodActivationResult
-from wakepy.core.activation import StageName
+from wakepy.core.activation import StageName, WakepyFakeSuccess
 
 PLATFORM_SUPPORT_FAIL = MethodActivationResult(
     success=False,
@@ -24,6 +24,18 @@ UNUSED_RESULT = MethodActivationResult(
     success=None,
     method_name="some-unused-method",
 )
+
+WAKEPY_FAKE_NOTINUSE = MethodActivationResult(
+    success=False,
+    failure_stage=StageName.ACTIVATION,
+    method_name=WakepyFakeSuccess.name,
+)
+WAKEPY_FAKE_SUCCESS = MethodActivationResult(
+    success=True,
+    failure_stage=StageName.ACTIVATION,
+    method_name=WakepyFakeSuccess.name,
+)
+
 METHODACTIVATIONRESULTS_1 = [
     PLATFORM_SUPPORT_FAIL,
     REQUIREMENTS_FAIL,
@@ -64,14 +76,14 @@ METHODACTIVATIONRESULTS_3_FAIL = [
 
 
 def test_activation_result_list_methods():
-    ar = ActivationResult()
-    ar._results = [
-        PLATFORM_SUPPORT_FAIL,
-        REQUIREMENTS_FAIL,
-        SUCCESS_RESULT,
-        UNUSED_RESULT,
-    ]
-
+    ar = ActivationResult(
+        [
+            PLATFORM_SUPPORT_FAIL,
+            REQUIREMENTS_FAIL,
+            SUCCESS_RESULT,
+            UNUSED_RESULT,
+        ]
+    )
     # By default, the list_methods drops out failures occuring in the
     # platform stage
     assert ar.list_methods() == [
@@ -104,13 +116,14 @@ def test_activation_result_list_methods():
 
 
 def test_activation_result_query():
-    ar = ActivationResult()
-    ar._results = [
-        PLATFORM_SUPPORT_FAIL,
-        REQUIREMENTS_FAIL,
-        SUCCESS_RESULT,
-        UNUSED_RESULT,
-    ]
+    ar = ActivationResult(
+        [
+            PLATFORM_SUPPORT_FAIL,
+            REQUIREMENTS_FAIL,
+            SUCCESS_RESULT,
+            UNUSED_RESULT,
+        ]
+    )
 
     # When no arguments given, return everything
     assert ar.query() == [
@@ -140,21 +153,21 @@ def test_activation_result_query():
 
 
 @pytest.mark.parametrize(
-    "results, success_expected, real_success_expected, faking_success",
+    "results, success_expected, real_success_expected",
     [
-        (METHODACTIVATIONRESULTS_1, True, True, "0"),
-        (METHODACTIVATIONRESULTS_2, True, True, "0"),
-        (METHODACTIVATIONRESULTS_3_FAIL, False, False, "0"),
-        (METHODACTIVATIONRESULTS_3_FAIL, True, False, "1"),
+        (METHODACTIVATIONRESULTS_1, True, True),
+        (METHODACTIVATIONRESULTS_2, True, True),
+        (METHODACTIVATIONRESULTS_3_FAIL + [WAKEPY_FAKE_NOTINUSE], False, False),
+        (METHODACTIVATIONRESULTS_3_FAIL + [WAKEPY_FAKE_SUCCESS], True, False),
     ],
 )
 def test_activation_result_success(
-    results, success_expected, real_success_expected, faking_success
+    results,
+    success_expected,
+    real_success_expected,
 ):
-    with pytest.MonkeyPatch.context() as mp:
-        mp.setenv("WAKEPY_FAKE_SUCCESS", str(faking_success))
-        ar = ActivationResult()
-        ar._results = results
+    with pytest.MonkeyPatch.context():
+        ar = ActivationResult(results)
 
         assert ar.success == success_expected
         assert ar.real_success == real_success_expected
