78951fddb8c00c5aee57e4a3e2fbd6de7681b2dc
Niko Föhr <fohrloop@gmail.com>
2024-01-19 17:26:42 +0200

    add tests for the unsuccessful case

--- tests/unit/test_methods/test_windows.py
+++ tests/unit/test_methods/test_windows.py
@@ -20,12 +20,10 @@ from wakepy.methods.windows import (
         (WindowsKeepRunning, ES_CONTINUOUS | ES_SYSTEM_REQUIRED),
     ],
 )
-def test_windows_setthreadexecutionstate_enter_mode(method_cls, expected_flag):
+def test_enter_mode_success(method_cls, expected_flag):
     method = method_cls()
 
-    with patch(
-        "wakepy.methods.windows.ctypes",
-    ) as ctypesmock:
+    with patch("wakepy.methods.windows.ctypes") as ctypesmock:
         retval = method.enter_mode()
 
     assert retval is None
@@ -36,13 +34,45 @@ def test_windows_setthreadexecutionstate_enter_mode(method_cls, expected_flag):
     "method_cls",
     [WindowsKeepPresenting, WindowsKeepRunning],
 )
-def test_windows_setthreadexecutionstate_exit_mode(method_cls):
+def test_exit_mode_success(method_cls):
     method = method_cls()
 
-    with patch(
-        "wakepy.methods.windows.ctypes",
-    ) as ctypesmock:
+    with patch("wakepy.methods.windows.ctypes") as ctypesmock:
         retval = method.exit_mode()
 
     assert retval is None
     ctypesmock.windll.kernel32.SetThreadExecutionState.assert_called_with(ES_CONTINUOUS)
+
+
+@pytest.mark.parametrize(
+    "method_cls",
+    [WindowsKeepPresenting, WindowsKeepRunning],
+)
+def test_enter_mode_with_exception(method_cls):
+    method = method_cls()
+
+    def raising_exc(_):
+        raise AttributeError("foo")
+
+    with patch("wakepy.methods.windows.ctypes") as ctypesmock:
+        ctypesmock.windll.kernel32.SetThreadExecutionState.side_effect = raising_exc
+
+        with pytest.raises(RuntimeError, match="Could not use kernel32.dll!"):
+            method.enter_mode()
+
+
+@pytest.mark.parametrize(
+    "method_cls",
+    [WindowsKeepPresenting, WindowsKeepRunning],
+)
+def test_exit_mode_with_exception(method_cls):
+    method = method_cls()
+
+    def raising_exc(_):
+        raise AttributeError("foo")
+
+    with patch("wakepy.methods.windows.ctypes") as ctypesmock:
+        ctypesmock.windll.kernel32.SetThreadExecutionState.side_effect = raising_exc
+
+        with pytest.raises(RuntimeError, match="Could not use kernel32.dll!"):
+            method.exit_mode()
