5a3e0d680262a743c265344c99ed2a31f31f09df
Niko Föhr <fohrloop@gmail.com>
2024-01-23 22:15:20 +0000

    update CLI + add tests for it (Issues 54 & 86) (#179)
    Remove the deprecated parts (-s/--keep-screen-awake flag)
    Make one-to-one mapping with the flags vs modes:
    
    -k -> keep.running
    -p -> keep.presenting
    
    No other options for CLI at this point.

---
+++ tests/unit/test_main.py
@@ -0,0 +1,151 @@
+"""Tests for the __main__ CLI"""
+from unittest.mock import MagicMock, Mock, call, patch
+
+import pytest
+
+from wakepy.__main__ import (
+    get_startup_text,
+    main,
+    parse_arguments,
+    wait_until_keyboardinterrupt,
+)
+from wakepy.core import Mode
+from wakepy.core.constants import ModeName
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["-k"],
+        ["--keep-running"],
+        # Also no args means keep running
+        [],
+    ],
+)
+def test_get_argparser_keep_running(args):
+    assert parse_arguments(args) == ModeName.KEEP_RUNNING
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["-p"],
+        ["--presentation"],
+    ],
+)
+def test_get_argparser_keep_presenting(args):
+    assert parse_arguments(args) == ModeName.KEEP_PRESENTING
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["-k", "-p"],
+        ["--presentation", "-k"],
+        ["-p", "--keep-running"],
+        ["--presentation", "--keep-running"],
+    ],
+)
+def test_get_argparser_too_many_modes(args):
+    with pytest.raises(ValueError, match="You may only select one of the modes!"):
+        assert parse_arguments(args)
+
+
+def test_get_startup_text_smoke_test():
+    assert isinstance(get_startup_text(ModeName.KEEP_PRESENTING), str)
+
+
+def test_wait_until_keyboardinterrupt():
+    def raise_keyboardinterrupt(_):
+        raise KeyboardInterrupt
+
+    with patch("wakepy.__main__.time") as timemock:
+        timemock.sleep.side_effect = raise_keyboardinterrupt
+
+        wait_until_keyboardinterrupt()
+
+
+def get_mocks_for_main(
+    parse_arguments,
+    create_mode,
+    get_startup_text,
+    wait_until_keyboardinterrupt,
+    mode_works: bool,
+):
+    cli_arg = Mock()
+    mockmodename = Mock(spec_set=ModeName.KEEP_PRESENTING)
+    mockprint = Mock()
+
+    class TestMode(Mode):
+        active = mode_works
+
+    mockmode = MagicMock(spec_set=TestMode)
+    mockmode.active = mode_works
+    sysarg = ["programname", cli_arg]
+    parse_arguments.return_value = mockmodename
+    get_startup_text.return_value = "startuptext"
+    create_mode.return_value = mockmode
+
+    mocks = Mock()
+    mocks.sysarg = sysarg
+    mocks.attach_mock(mockprint, "print")
+    mocks.attach_mock(mockmode, "mode")
+    mocks.attach_mock(parse_arguments, "parse_arguments")
+    mocks.attach_mock(create_mode, "create_mode")
+    mocks.attach_mock(get_startup_text, "get_startup_text")
+    mocks.attach_mock(wait_until_keyboardinterrupt, "wait_until_keyboardinterrupt")
+    return mocks
+
+
+@patch("wakepy.__main__.wait_until_keyboardinterrupt")
+@patch("wakepy.__main__.get_startup_text")
+@patch("wakepy.__main__.create_mode")
+@patch("wakepy.__main__.parse_arguments")
+def test_main(
+    parse_arguments, create_mode, get_startup_text, wait_until_keyboardinterrupt
+):
+    """This is just a smoke test for the main() function. It checks that
+    correct functions are called in the correct order and correct arguments,
+    but the functionality of each of the functions is tested elsewhere."""
+
+    mocks = get_mocks_for_main(
+        parse_arguments,
+        create_mode,
+        get_startup_text,
+        wait_until_keyboardinterrupt,
+        mode_works=True,
+    )
+
+    with patch("sys.argv", mocks.sysarg), patch("builtins.print", mocks.print):
+        main()
+
+    assert mocks.mock_calls == [
+        call.parse_arguments(mocks.sysarg[1:]),
+        call.create_mode(modename=parse_arguments.return_value),
+        call.mode.__enter__(),
+        call.get_startup_text(mode=parse_arguments.return_value),
+        call.print(get_startup_text.return_value),
+        call.wait_until_keyboardinterrupt(),
+        call.mode.__exit__(None, None, None),
+        call.print("\nExited."),
+    ]
+
+
+@patch("wakepy.__main__.wait_until_keyboardinterrupt")
+@patch("wakepy.__main__.get_startup_text")
+@patch("wakepy.__main__.create_mode")
+@patch("wakepy.__main__.parse_arguments")
+def test_main_with_non_working_mode(
+    parse_arguments, create_mode, get_startup_text, wait_until_keyboardinterrupt
+):
+    mocks = get_mocks_for_main(
+        parse_arguments,
+        create_mode,
+        get_startup_text,
+        wait_until_keyboardinterrupt,
+        mode_works=False,
+    )
+
+    with patch("sys.argv", mocks.sysarg), patch("builtins.print", mocks.print):
+        with pytest.raises(RuntimeError, match="Could not activate"):
+            main()
--- wakepy/__main__.py
+++ wakepy/__main__.py
@@ -1,4 +1,4 @@
-"""Start wakepy keepawake with CLI
+"""This module defines the CLI for wakepy
 
 This is called either with
 
@@ -8,13 +8,70 @@ or using the executable
 
     wakepy [args]
 """
+from __future__ import annotations
+
 import argparse
-from typing import Dict
+import itertools
+import sys
+import time
+import typing
 
-from wakepy.cli import start
+from wakepy.core.constants import ModeName
+from wakepy.core.mode import create_mode
+
+if typing.TYPE_CHECKING:
+    from typing import List
+
+WAKEPY_TEXT_TEMPLATE = r"""                  _                       
+                 | |                      
+ __      __ __ _ | | __ ___  _ __   _   _ 
+ \ \ /\ / // _` || |/ // _ \| '_ \ | | | |
+  \ V  V /| (_| ||   <|  __/| |_) || |_| |
+   \_/\_/  \__,_||_|\_\\___|| .__/  \__, |
+{VERSION_STRING}        | |      __/ |
+                            |_|     |___/ """
+
+WAKEPY_TICKBOXES_TEMPLATE = """
+ [{no_auto_suspend}] System will continue running programs
+ [{presentation_mode}] Presentation mode is on 
+"""
 
 
-def get_argparser() -> argparse.ArgumentParser:
+def main():
+    modename = parse_arguments(sys.argv[1:])
+    mode = create_mode(modename=modename)
+    with mode:
+        if not mode.active:
+            raise RuntimeError("Could not activate")
+        print(get_startup_text(mode=modename))
+        wait_until_keyboardinterrupt()
+
+    print("\nExited.")
+
+
+def parse_arguments(
+    sysargs: List[str],
+) -> ModeName:
+    """Parses arguments from sys.argv and returns kwargs for"""
+
+    args = _get_argparser().parse_args(sysargs)
+
+    n_flags_selected = sum((args.keep_running, args.presentation))
+
+    if n_flags_selected > 1:
+        raise ValueError('You may only select one of the modes! See: "wakepy -h"')
+
+    if args.keep_running or n_flags_selected == 0:
+        # The default action, if nothing is selected, is "keep running"
+        mode = ModeName.KEEP_RUNNING
+    else:
+        assert args.presentation
+        mode = ModeName.KEEP_PRESENTING
+
+    return mode
+
+
+def _get_argparser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="wakepy",
         formatter_class=lambda prog: argparse.HelpFormatter(
@@ -24,68 +81,57 @@ def get_argparser() -> argparse.ArgumentParser:
         ),
     )
 
-    def add_mode(short, long, help, default=False):
-        parser.add_argument(
-            short, long, help=help, action="store_true", default=default
-        )
-
-    add_mode(
+    parser.add_argument(
         "-k",
         "--keep-running",
         help=(
-            "Keep programs running; inhibit automatic sleep/suspend. This is"
-            " used as a default if no modes are selected."
+            "Keep programs running; inhibit automatic idle timer based sleep / "
+            "suspend. If a screen lock (or a screen saver) with a password is enabled, "
+            "your system *may* still lock the session automatically. You may, and "
+            "probably should, lock the session manually. Locking the workstation does "
+            "not stop programs from executing. This is used as the default if no modes "
+            "are selected."
         ),
+        action="store_true",
+        default=False,
     )
 
-    add_mode(
+    parser.add_argument(
         "-p",
         "--presentation",
-        help=("Presentation mode; inhibit automatic sleep, screensaver and screenlock"),
-    )
-    add_mode(
-        "-s",
-        "--keep-screen-awake",
         help=(
-            "[DEPRECATED] Keep also the screen awake. "
-            "On Linux, this flag is set on and cannot be disabled."
+            "Presentation mode; inhibit automatic idle timer based sleep, screensaver, "
+            "screenlock and display power management."
         ),
+        action="store_true",
+        default=False,
     )
+
     return parser
 
 
-def parse_arguments(parser: argparse.ArgumentParser) -> Dict[str, bool]:
-    args = parser.parse_args()
+def get_startup_text(mode: ModeName) -> str:
+    from wakepy import __version__
 
-    if args.keep_screen_awake and any((args.keep_running, args.presentation)):
-        raise ValueError(
-            "The deprecated --keep-screen-awake option cannot be used with the new"
-            " options!"
-        )
-
-    if args.keep_screen_awake:
-        # Deprecated in 0.7.0 -> remove in later release.
-        deprecation_warning = True
-        args.presentation = args.keep_screen_awake
-    else:
-        deprecation_warning = False
-
-    if not any((args.keep_running, args.presentation, args.keep_screen_awake)):
-        # The default action, if nothing is selected, is "keep running"
-        args.keep_running = True
-
-    return dict(
-        keep_running=args.keep_running,
-        presentation_mode=args.presentation,
-        deprecation_warning=deprecation_warning,
+    wakepy_text = WAKEPY_TEXT_TEMPLATE.format(
+        VERSION_STRING=f"{'  v.'+__version__: <20}"
     )
+    options_txt = WAKEPY_TICKBOXES_TEMPLATE.strip("\n").format(
+        no_auto_suspend="x",
+        presentation_mode="x" if mode == ModeName.KEEP_PRESENTING else " ",
+    )
+    return "\n".join((wakepy_text, options_txt)) + "\n"
 
 
-def main():
-    parser = get_argparser()
-    kwargs = parse_arguments(parser)
-    start(**kwargs)
+def wait_until_keyboardinterrupt():
+    spinning_chars = ["|", "/", "-", "\\"]
+    try:
+        for char in itertools.cycle(spinning_chars):  # pragma: no branch
+            print("\r" + char + r" [Press Ctrl+C to exit]", end="")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
-    main()
+    main()  # pragma: no cover
