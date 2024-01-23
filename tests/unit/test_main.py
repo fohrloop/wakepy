45ed94d99ecfe5caff10be6abe25ac3cd1b574e8
Niko Föhr <fohrloop@gmail.com>
2024-01-23 22:02:59 +0200

    add tests for cli args parsing

---
+++ tests/unit/test_main.py
@@ -0,0 +1,43 @@
+"""Tests for the __main__ CLI"""
+import pytest
+
+from wakepy.__main__ import parse_arguments
+from wakepy.core.constants import ModeName
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["-k"],
+        ["--keep-running"],
+        # Also no args means keep running
+        [],
+    ],
+)
+def test_get_argparser_keep_running(args):
+    assert parse_arguments(args) == dict(modename=ModeName.KEEP_RUNNING)
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["-p"],
+        ["--presentation"],
+    ],
+)
+def test_get_argparser_keep_running(args):
+    assert parse_arguments(args) == dict(modename=ModeName.KEEP_PRESENTING)
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["-k", "-p"],
+        ["--presentation", "-k"],
+        ["-p", "--keep-running"],
+        ["--presentation", "--keep-running"],
+    ],
+)
+def test_get_argparser_keep_running(args):
+    with pytest.raises(ValueError, match="You may only select one of the modes!"):
+        assert parse_arguments(args)
