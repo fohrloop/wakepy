e58d74fd6440ad156d26574ed6c6a0cc881ac40f
Niko Föhr <fohrloop@gmail.com>
2024-01-23 22:39:46 +0200

    improve __main__ test coverage

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,7 +1,12 @@
 """Tests for the __main__ CLI"""
 import pytest
 
-from wakepy.__main__ import parse_arguments
+from unittest.mock import patch
+from wakepy.__main__ import (
+    parse_arguments,
+    get_startup_text,
+    wait_until_keyboardinterrupt,
+)
 from wakepy.core.constants import ModeName
 
 
@@ -41,3 +46,17 @@ def test_get_argparser_keep_presenting(args):
 def test_get_argparser_too_many_modes(args):
     with pytest.raises(ValueError, match="You may only select one of the modes!"):
         assert parse_arguments(args)
+
+
+def test_get_startup_text_smoke_test():
+    assert isinstance(get_startup_text(ModeName.KEEP_PRESENTING), str)
+
+
+def test_wait_until_keyboardinterrupt():
+    def raise_keyboardinterrupt(_):
+        raise KeyboardInterrupt
+
+    with patch("wakepy.__main__.time") as timemock:
+        timemock.sleep.side_effect = raise_keyboardinterrupt
+
+        wait_until_keyboardinterrupt()
