5d9fc45cfbe5324809589f12a77f59aed3953b47
Niko Föhr <fohrloop@gmail.com>
2024-01-23 23:13:08 +0200

    add test for main()

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,10 +1,11 @@
 """Tests for the __main__ CLI"""
-from unittest.mock import patch
+from unittest.mock import patch, Mock, call
 
 import pytest
 
 from wakepy.__main__ import (
     get_startup_text,
+    main,
     parse_arguments,
     wait_until_keyboardinterrupt,
 )
@@ -61,3 +62,43 @@ def test_wait_until_keyboardinterrupt():
         timemock.sleep.side_effect = raise_keyboardinterrupt
 
         wait_until_keyboardinterrupt()
+
+
+@patch("wakepy.__main__.wait_until_keyboardinterrupt")
+@patch("wakepy.__main__.get_startup_text")
+@patch("wakepy.__main__.create_mode")
+@patch("wakepy.__main__.parse_arguments")
+def test_main(
+    parse_arguments, create_mode, get_startup_text, wait_until_keyboardinterrupt
+):
+    """This is just a smoke test for the main() function. It checks that
+    correct functions are called in the correct order and correct arguments,
+    but the functionality of each of the functions is tested elsewhere."""
+    cli_arg = Mock()
+    mockmodename = Mock(spec_set=ModeName.KEEP_PRESENTING)
+    mockprint = Mock()
+    sysarg = ["programname", cli_arg]
+    parse_arguments.return_value = dict(modename=mockmodename)
+    get_startup_text.return_value = "startuptext"
+
+    mocks = Mock()
+    mocks.attach_mock(mockprint, "print")
+    mocks.attach_mock(parse_arguments, "parse_arguments")
+    mocks.attach_mock(create_mode, "create_mode")
+    mocks.attach_mock(get_startup_text, "get_startup_text")
+    mocks.attach_mock(wait_until_keyboardinterrupt, "wait_until_keyboardinterrupt")
+
+    with patch("sys.argv", sysarg), patch("builtins.print", mockprint):
+        main()
+
+    assert mocks.mock_calls == [
+        call.parse_arguments([cli_arg]),
+        call.create_mode(modename=mockmodename),
+        call.create_mode().__enter__(),
+        call.create_mode().active.__bool__(),
+        call.get_startup_text(mode=mockmodename),
+        call.print(get_startup_text.return_value),
+        call.wait_until_keyboardinterrupt(),
+        call.create_mode().__exit__(None, None, None),
+        call.print("\nExited."),
+    ]
