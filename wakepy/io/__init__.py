d8af7b11c9eac0557d6478bd0715ac0a7d79c31c
Niko Föhr <fohrloop@gmail.com>
2024-01-23 22:27:21 +0000

    move dbus_adapters package (#180)
    Flatten the package structure.
    
    was: wakepy.io.dbus
    now: wakepy.dbus_adapters
    
    remove the old wakepy.io package, as there is no other use for it currently than the dbus_adapters.

--- wakepy/io/__init__.py
+++
@@ -1,7 +0,0 @@
-"""This sub-package contains most[1] of the parts of wakepy which do I/O;
-
-* Implementation of dbus adapters
-
-[1] There are some environment variable checks and reading of the platform
-version in the core. 
-"""
