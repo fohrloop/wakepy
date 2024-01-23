e52822cb608580961e05b65b7ebe92db1658cf2f
Niko Föhr <fohrloop@gmail.com>
2024-01-23 20:16:48 +0200

    remove deprecated stuff

--- wakepy/__main__.py
+++ wakepy/__main__.py
@@ -43,32 +43,12 @@ def get_argparser() -> argparse.ArgumentParser:
         "--presentation",
         help=("Presentation mode; inhibit automatic sleep, screensaver and screenlock"),
     )
-    add_mode(
-        "-s",
-        "--keep-screen-awake",
-        help=(
-            "[DEPRECATED] Keep also the screen awake. "
-            "On Linux, this flag is set on and cannot be disabled."
-        ),
-    )
+
     return parser
 
 
 def parse_arguments(parser: argparse.ArgumentParser) -> Dict[str, bool]:
     args = parser.parse_args()
-
-    if args.keep_screen_awake and any((args.keep_running, args.presentation)):
-        raise ValueError(
-            "The deprecated --keep-screen-awake option cannot be used with the new"
-            " options!"
-        )
-
-    if args.keep_screen_awake:
-        # Deprecated in 0.7.0 -> remove in later release.
-        deprecation_warning = True
-        args.presentation = args.keep_screen_awake
-    else:
-        deprecation_warning = False
 
     if not any((args.keep_running, args.presentation, args.keep_screen_awake)):
         # The default action, if nothing is selected, is "keep running"
@@ -77,7 +57,6 @@ def parse_arguments(parser: argparse.ArgumentParser) -> Dict[str, bool]:
     return dict(
         keep_running=args.keep_running,
         presentation_mode=args.presentation,
-        deprecation_warning=deprecation_warning,
     )
 
 
