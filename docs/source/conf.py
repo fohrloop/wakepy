e2f51e0ad39b9dcbdf04ff6e622327adcb8c089f
Niko Föhr <fohrloop@gmail.com>
2024-01-31 23:00:04 +0200

    remove fullscreen button

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,6 +88,7 @@ html_theme_options = {
     "repository_url": "https://github.com/fohrloop/wakepy",
     "use_repository_button": True,
     "use_download_button": False,
+    "use_fullscreen_button": False,
     # Shows the landing page in the sidebar
     # See: https://sphinx-book-theme.readthedocs.io/en/stable/sections/sidebar-primary.html#add-the-home-page-to-your-table-of-contents
     "home_page_in_toc": True,
