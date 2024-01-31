f6efeee9f496c8d0548d6c52aaf1e2f556536451
Niko Föhr <fohrloop@gmail.com>
2024-01-31 21:07:04 +0200

    change furo to sphinx-book-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "sphinx_book_theme"
 
 html_theme_options = {
     "footer_icons": [
