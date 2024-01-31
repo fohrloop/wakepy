49fa53f657717649e7a26f06d2baa1917c6a44c6
Niko Föhr <fohrloop@gmail.com>
2024-01-31 21:59:36 +0200

    show the landing page in the sidebar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,4 +87,7 @@ html_theme = "sphinx_book_theme"
 html_theme_options = {
     "repository_url": "https://github.com/fohrloop/wakepy",
     "use_repository_button": True,
+    # Shows the landing page in the sidebar
+    # See: https://sphinx-book-theme.readthedocs.io/en/stable/sections/sidebar-primary.html#add-the-home-page-to-your-table-of-contents
+    "home_page_in_toc": True,
 }
