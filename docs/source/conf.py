a6131732c5c68484ef59493243af69a2f88c3a4d
Niko Föhr <fohrloop@gmail.com>
2024-02-08 01:02:14 +0200

    fix problem with bullet points in docs
    For some reason the docs showed extra bullet point lists when creating
    autodocs. The Parameters list was having a bullet point list.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,6 +69,9 @@ napoleon_google_docstring = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+html_static_path = ["_static"]
+html_css_files = ["wakepy-docs.css"]
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
