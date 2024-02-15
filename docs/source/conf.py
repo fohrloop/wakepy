83fe9795357761152023d899c9a6379512d016a2
Niko Föhr <fohrloop@gmail.com>
2024-02-15 23:43:18 +0200

    remove css (not used)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,9 +63,7 @@ numfig = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
 html_static_path = ["_static"]
-html_css_files = ["wakepy-docs.css"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
