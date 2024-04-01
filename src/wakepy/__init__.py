3de7cfb8f6d3630e4963fc8b673d9b794d7b4760
Niko Föhr <fohrloop@gmail.com>
2024-04-01 21:03:45 +0300

    make setuptools-scm to set __version__

--- src/wakepy/__init__.py
+++ src/wakepy/__init__.py
@@ -1,5 +1,16 @@
 # NOTE The methods sub-package is imported for registering all the methods.
 from . import methods as methods
+
+try:
+    from ._version import __version__ as __version__
+    from ._version import version_tuple as version_tuple
+except ImportError:  # pragma: no cover
+    # Likely an editable install. Should ever happen if installed from a
+    # distribution package (sdist or wheel)
+    __version__ = "undefined"
+    version_tuple = (0, 0, 0, "undefined")
+
+
 from .core import ActivationError as ActivationError
 from .core import ActivationResult as ActivationResult
 from .core import DBusAdapter as DBusAdapter
@@ -7,5 +18,3 @@ from .core import Method as Method
 from .core import Mode as Mode
 from .core import ModeExit as ModeExit
 from .modes import keep as keep
-
-__version__ = "0.8.0.dev0"
