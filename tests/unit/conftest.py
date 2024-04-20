d0aabce99b2fbe5f45262b6e5f1fb9de2cb31652
Niko Föhr <fohrloop@gmail.com>
2024-04-20 13:02:18 +0300

    rename _fake to _testing

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from wakepy import DBusAdapter
 from wakepy.core.registry import register_method
-from wakepy.methods._fake import WakepyFakeSuccess
+from wakepy.methods._testing import WakepyFakeSuccess
 
 
 class TestDBusAdapter(DBusAdapter):
