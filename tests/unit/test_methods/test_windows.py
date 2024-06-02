a3721db525dfafb8be334f8c7c30a3f18f35395c
Niko Föhr <fohrloop@gmail.com>
2024-06-02 19:46:48 +0300

    test coverage to 100%

--- tests/unit/test_methods/test_windows.py
+++ tests/unit/test_methods/test_windows.py
@@ -1,5 +1,7 @@
 from unittest.mock import patch
 
+
+import re
 import pytest
 
 from wakepy.methods.windows import (
@@ -103,3 +105,38 @@ class TestWindowsSetThreadExecutionState:
         with patch("wakepy.methods.windows.ctypes.windll.kernel32.SetThreadExecutionState", lambda x:0):
             with pytest.raises(RuntimeError, match="SetThreadExecutionState returned NULL"):
                 method.exit_mode()
+
+
+    @pytest.mark.parametrize(
+        "method_cls",
+        [WindowsKeepPresenting, WindowsKeepRunning],
+    )
+    def test_exit_mode_before_enter(self, method_cls):
+        # does not make much practical sense but required for test coverage
+        method = method_cls()
+        # need to add something to the queue; otherwise would wait until an exception
+        method._queue_from_thread.put(1)
+        method.exit_mode()
+        assert method._inhibiting_thread is None 
+
+
+
+    @pytest.mark.parametrize(
+        "method_cls, expected_flag",
+        [
+            (
+                WindowsKeepPresenting,
+                ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED,
+            ),
+            (WindowsKeepRunning, ES_CONTINUOUS | ES_SYSTEM_REQUIRED),
+        ],
+    )
+    def test_wrong_return_type_from_set_thread_execution_state(self, method_cls, expected_flag):
+        method = method_cls()
+
+        def set_thread_execution_state(flags):
+            return 'foo'
+
+        with patch("wakepy.methods.windows.ctypes.windll.kernel32.SetThreadExecutionState", set_thread_execution_state):
+            with pytest.raises(RuntimeError, match=re.escape("Unknown result type: <class 'str'> (foo)")):
+                method.enter_mode()
