fohrloop/wakepy

commit 6b3b0f89199ee47fa0cf0e1ca1a3586c47d43558

Change how platform support is defined (#379)
Closes: #377

Summary
=======
* Replace PlatformName with PlatformType and IdentifiedPlatformType
* If platform is unknown, do not fail in the platform check
  phase anymore, but continue trying using the method.
  

Before
======
PlatformName constant, which is used to both: (1) As type for current
platform (2) As types to list in subclasses of Method in the 
supported_platforms attribute.

The problem was that the supported platforms definition was not very
flexible. For example when adding support for FreeBSD (GhostBSD), in
https://github.com/fohrloop/wakepy/issues/359, one would have needed
to add the PlatformName.FREEBSD to each of the methods, like this:

class SomeMethod(Method):
    supported_platforms = (PlatformName.LINUX, PlatformName.FREEBSD) 


Now
===

Now there's the possibility to use

class SomeMethod(Method):
    supported_platforms = (PlatformType.UNIX_LIKE_FOSS, ) 

where the UNIX_LIKE_FOSS contains all FOSS Unix-like systems (linux+bsd,
but not MacOS or Android). Adding now systems to the list does not
require any changes in the Method subclasses supporting UNIX_LIKE_FOSS.

There are now two types of constants: (1) IdentifiedPlatformType, which
is used as the type of the current platform and (2) PlatformType, which
is used in the subclasses of Method in the supported_platforms
attribute.

New PlatformTypes
-----------------
PlatformType.BSD: Any BSD system. Currently only consists of FreeBSD but
the list may grow in the future.

PlatformType.UNIX_LIKE_FOSS: Unix-like desktop environment, but FOSS.
Includes: Linux and BSD. Excludes: Android (mobile), MacOS (non-FOSS),
ChromeOS (non-FOSS)

PlatformType.ANY: Any platform.


New behavior
------------
Platform support check does not fail (but returns "I don't know"/None)
if the platform could not be detected. This means that methods like

tests/unit/test_core/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 from tests.unit.test_core.testmethods import TestMethod
-from wakepy.core import DBusAddress, DBusMethod, Method, PlatformName
+from wakepy.core import DBusAddress, DBusMethod, Method, PlatformType
 from wakepy.core.heartbeat import Heartbeat
 
 
@@ -28,34 +28,34 @@ def provide_methods_different_platforms(monkeypatch, testutils):
 
     class WindowsA(TestMethod):
         name = "WinA"
-        supported_platforms = (PlatformName.WINDOWS,)
+        supported_platforms = (PlatformType.WINDOWS,)
 
     class WindowsB(TestMethod):
         name = "WinB"
-        supported_platforms = (PlatformName.WINDOWS,)
+        supported_platforms = (PlatformType.WINDOWS,)
 
     class WindowsC(TestMethod):
         name = "WinC"
-        supported_platforms = (PlatformName.WINDOWS,)
+        supported_platforms = (PlatformType.WINDOWS,)
 
     class LinuxA(TestMethod):
         name = "LinuxA"
-        supported_platforms = (PlatformName.LINUX,)
+        supported_platforms = (PlatformType.LINUX,)
 
     class LinuxB(TestMethod):
         name = "LinuxB"
-        supported_platforms = (PlatformName.LINUX,)
+        supported_platforms = (PlatformType.LINUX,)
 
     class LinuxC(TestMethod):
         name = "LinuxC"
-        supported_platforms = (PlatformName.LINUX,)
+        supported_platforms = (PlatformType.LINUX,)
 
     class MultiPlatformA(TestMethod):
         name = "multiA"
         supported_platforms = (
-            PlatformName.LINUX,
-            PlatformName.WINDOWS,
-            PlatformName.MACOS,
+            PlatformType.LINUX,
+            PlatformType.WINDOWS,
+            PlatformType.MACOS,
         )
 
 

tests/unit/test_core/test_prioritization.py
@@ -5,7 +5,7 @@ import typing
 
 import pytest
 
-from wakepy.core import PlatformName
+from wakepy.core import PlatformType
 from wakepy.core.constants import WAKEPY_FAKE_SUCCESS
 from wakepy.core.prioritization import (
     _check_methods_priority,
@@ -25,7 +25,7 @@ if typing.TYPE_CHECKING:
 def set_current_platform_to_linux(monkeypatch):
 
     monkeypatch.setattr(
-        "wakepy.core.prioritization.CURRENT_PLATFORM", PlatformName.LINUX
+        "wakepy.core.prioritization.CURRENT_PLATFORM", PlatformType.LINUX
     )
 
 
@@ -33,7 +33,8 @@ def set_current_platform_to_linux(monkeypatch):
 def set_current_platform_to_windows(monkeypatch):
 
     monkeypatch.setattr(
-        "wakepy.core.prioritization.CURRENT_PLATFORM", PlatformName.WINDOWS
+        "wakepy.core.prioritization.CURRENT_PLATFORM",
+        PlatformType.WINDOWS,
     )
 
 

src/wakepy/__main__.py
@@ -69,6 +69,7 @@ def handle_activation_error(result: ActivationResult) -> None:
 def _get_activation_error_text(result: ActivationResult) -> str:
     from wakepy import __version__
 
+    # LATER: This should be improved in https://github.com/fohrloop/wakepy/issues/378
     error_text = f"""
     Wakepy could not activate the "{result.mode_name}" mode. This might occur because of a bug or because your current platform is not yet supported or your system is missing required software.
 

src/wakepy/core/__init__.py
@@ -7,8 +7,9 @@ See the public Python API at: https://wakepy.readthedocs.io/
 from .activationresult import ActivationResult as ActivationResult
 from .activationresult import MethodActivationResult as MethodActivationResult
 from .constants import BusType as BusType
+from .constants import IdentifiedPlatformType as IdentifiedPlatformType
 from .constants import ModeName as ModeName
-from .constants import PlatformName as PlatformName
+from .constants import PlatformType as PlatformType
 from .dbus import DBusAdapter as DBusAdapter
 from .dbus import DBusAddress as DBusAddress
 from .dbus import DBusMethod as DBusMethod

src/wakepy/core/constants.py
@@ -22,22 +22,62 @@ falsy.
 """
 
 
-class PlatformName(StrEnum):
-    """All the different platforms wakepy knows about. Any platform that is not
-    detected will be named ``OTHER``."""
+class IdentifiedPlatformType(StrEnum):
+    """The type identifier for the (:attr:`~wakepy.core.platform.CURRENT_PLATFORM`).
+    Any process will be categorized into exactly one
+    :class:`IdentifiedPlatformType` type; these are mutually exclusive options.
+    Any platform that is not detected will be labeled as ``UNKNOWN``.
+
+    See also: :class:`PlatformType`, which you should use with Method
+    subclasses."""  # noqa: W505
 
     WINDOWS = auto()
-
     LINUX = auto()
-
     MACOS = auto()
+    FREEBSD = auto()
+    UNKNOWN = auto()
+
+
+class PlatformType(StrEnum):
+    """Enumeration for supported platform types. Each identified platform can
+    be categorized to at least one, but potentially many of these. In other
+    words, each :class:`IdentifiedPlatformType` (type of
+    :attr:`~wakepy.core.platform.CURRENT_PLATFORM`) maps into one or many
+    ``PlatformType``.
+
+    To be used in subclasses of :class:`~wakepy.Method` in the
+    :meth:`~wakepy.Method.supported_platforms`.
+    """
+
+    # All of the IdentifiedPlatformType are always part of the PlatformType
+    # (also enforced with a test)
+    WINDOWS = IdentifiedPlatformType.WINDOWS.value
+    """Any Windows version from Windows 10 onwards."""
+
+    LINUX = IdentifiedPlatformType.LINUX.value
+    """Includes any Linux distro. Excludes things like  Android &  ChromeOS"""
+
+    MACOS = IdentifiedPlatformType.MACOS.value
     """Mac OS (Darwin)"""
 
-    OTHER = auto()
-    """Anything else"""
+    FREEBSD = IdentifiedPlatformType.FREEBSD.value
+    """FreeBSD. Also includes GhostBSD"""
 
+    UNKNOWN = IdentifiedPlatformType.UNKNOWN.value
+    """Any non-identified platform"""
 
-PlatformNameValue = Literal["WINDOWS", "LINUX", "MACOS", "OTHER"]
+    # These are special, combined platform types that make it easier to
+    # define the MethodSubclass.supported_platforms.
+    BSD = auto()
+    """Any BSD system (Currently just FreeBSD / GhostBSD, but is likely to
+    change in the future)."""
+
+    UNIX_LIKE_FOSS = auto()
+    """Unix-like desktop environment, but FOSS. Includes: LINUX and BSD.
+    Excludes: Android (mobile), MacOS (non-FOSS), ChromeOS (non-FOSS)."""
+
+    ANY = auto()
+    """Means any platform."""
 
 
 class ModeName(StrEnum):

src/wakepy/core/method.py
@@ -14,9 +14,9 @@ from abc import ABC
 from typing import Type, cast
 
 from .activationresult import MethodActivationResult
-from .constants import PlatformName, StageName
+from .constants import PlatformType, StageName
 from .heartbeat import Heartbeat
-from .platform import CURRENT_PLATFORM
+from .platform import CURRENT_PLATFORM, get_platform_supported
 from .registry import register_method
 from .strenum import StrEnum, auto
 
@@ -31,7 +31,7 @@ if typing.TYPE_CHECKING:
 
     from wakepy.core import DBusAdapter, DBusMethodCall
 
-    from .constants import ModeName, PlatformName
+    from .constants import ModeName, PlatformType
 
 MethodCls = Type["Method"]
 
@@ -69,20 +69,26 @@ class Method(ABC):
     defines the Mode `foo` (:class:`Mode` classes are themselves not defined or
     registered anywhere)"""
 
-    supported_platforms: Tuple[PlatformName, ...] = (
-        PlatformName.LINUX,
-        PlatformName.WINDOWS,
-        PlatformName.MACOS,
-        PlatformName.OTHER,
-    )
-    """Lists the platforms the Method supports. If a platform is not listed in
-    ``method.supported_platforms``, the ``method`` is not going to be used on
-    the platform (when used as part of  a :class:`Mode`), and the Method
-    activation result will show a fail in the "PLATFORM" stage.
+    supported_platforms: Tuple[PlatformType, ...] = (PlatformType.ANY,)
+    """Lists the platforms the Method supports. If the current platform is not
+    part of any of the platform types listed in ``method.supported_platforms``,
+    the ``method`` is not* going to be used (when used as part of a
+    :class:`Mode`), and the Method activation result will show a fail in the
+    "PLATFORM" stage.
 
-    When subclassing, defining ``supported_platforms`` reduces some work
-    required when writing the logic for :meth:`caniuse`.  Additionally, it aids
-    in distinguishing the "PLATFORM" stage fail as a separate type of failure.
+    When subclassing the ``Method``, defining ``supported_platforms`` reduces
+    some work required when writing the logic for :meth:`caniuse`.
+    Additionally, it aids in distinguishing the "PLATFORM" stage fail as a
+    separate type of failure.
+
+    As an example, if the Method would support all Unix-like FOSS desktop
+    operating systems, the supported_platforms would be
+    ``(PlatformType.UNIX_LIKE_FOSS, )``. See the
+    :class:`~wakepy.core.constants.PlatformType` for all options.
+
+    \*unless the current platform is unidentified (``UNKNOWN``). In this case,
+    platform check does not fail (nor succeed), and the activation process
+    continues normally.
 
     Default: Support all platforms.
     """
@@ -114,6 +120,7 @@ class Method(ABC):
 
         # waits for https://github.com/fohrloop/wakepy/issues/256
         # self.method_kwargs = kwargs
+        _check_supported_platforms(self.supported_platforms, self.__class__.__name__)
 
     def __init_subclass__(cls, **kwargs: object) -> None:
         register_method(cls)
@@ -285,6 +292,22 @@ class Method(ABC):
         return cls.name == unnamed
 
 
+def _check_supported_platforms(
+    supported_platforms: Tuple[PlatformType, ...], classname: str
+) -> None:
+    err_supported_platforms = (
+        f"The supported_platforms of {classname} must be a tuple of PlatformType!"
+    )
+
+    if not isinstance(supported_platforms, tuple):
+        raise ValueError(err_supported_platforms)
+    for p in supported_platforms:
+        if not isinstance(p, PlatformType):
+            raise ValueError(
+                err_supported_platforms + f' One item ({p}) is of type "{type(p)}"'
+            )
+
+
 def activate_method(method: Method) -> Tuple[MethodActivationResult, Heartbeat | None]:
     """Activates a mode defined by a single Method.
 
@@ -303,7 +326,7 @@ def activate_method(method: Method) -> Tuple[MethodActivationResult, Heartbeat |
         success=False, method_name=method.name, mode_name=method.mode_name
     )
 
-    if not get_platform_supported(method, platform=CURRENT_PLATFORM):
+    if get_platform_supported(CURRENT_PLATFORM, method.supported_platforms) is False:
         result.failure_stage = StageName.PLATFORM_SUPPORT
         return result, None
 
@@ -370,24 +393,6 @@ def deactivate_method(method: Method, heartbeat: Optional[Heartbeat] = None) -> 
 
     if method.dbus_adapter:
         method.dbus_adapter.close_connections()
-
-
-def get_platform_supported(method: Method, platform: PlatformName) -> bool:
-    """Checks if method is supported by the platform
-
-    Parameters
-    ----------
-    method: Method
-        The method which platform support to check.
-    platform:
-        The platform to check against.
-
-    Returns
-    -------
-    is_supported: bool
-        If True, the platform is supported. Otherwise, False.
-    """
-    return platform in method.supported_platforms
 
 
 def caniuse_fails(method: Method) -> tuple[bool, str]:

src/wakepy/core/platform.py
@@ -1,23 +1,103 @@
+from __future__ import annotations
+
 import platform
+import typing
 import warnings
 
-from .constants import PlatformName
+from .constants import IdentifiedPlatformType, PlatformType
+
+if typing.TYPE_CHECKING:
+    from typing import Callable, Tuple
+
+    PlatformFunc = Callable[[IdentifiedPlatformType], bool]
 
 
-def get_current_platform() -> PlatformName:
+def get_current_platform() -> IdentifiedPlatformType:
     # Ref: https://docs.python.org/3/library/platform.html#platform.system
     system = platform.system()
     if system == "Windows":
-        return PlatformName.WINDOWS
+        return IdentifiedPlatformType.WINDOWS
     elif system == "Darwin":
-        return PlatformName.MACOS
+        return IdentifiedPlatformType.MACOS
     elif system == "Linux":
-        return PlatformName.LINUX
+        return IdentifiedPlatformType.LINUX
+    elif system == "FreeBSD":
+        return IdentifiedPlatformType.FREEBSD
 
+    # LATER: This should be improved in https://github.com/fohrloop/wakepy/issues/378
     warnings.warn(
         f"Could not detect current platform! platform.system() returned {system}"
     )
-    return PlatformName.OTHER
+    return IdentifiedPlatformType.UNKNOWN
 
 
-CURRENT_PLATFORM = get_current_platform()
+CURRENT_PLATFORM: IdentifiedPlatformType = get_current_platform()
+"""The current platform as detected. If the platform cannot be detected,
+defaults to ``UNKNOWN``."""
+
+
+def get_platform_supported(
+    platform: IdentifiedPlatformType, supported_platforms: Tuple[PlatformType, ...]
+) -> bool | None:
+    """Checks if a platform is in the supported platforms.
+
+    Parameters
+    ----------
+    platform: IdentifiedPlatformType
+        The platform to check.
+    supported_platforms:
+        The platforms to check against.
+
+    Returns
+    -------
+    is_supported: bool | None
+        If platform is supported, returns True. If the support is unknown,
+        returns None, and if the platform is not supported, returns False.
+    """
+    for supported_platform in supported_platforms:
+        func = PLATFORM_INFO_FUNCS[supported_platform]
+        if func(platform) is True:
+            return True
+    if is_unknown(platform):
+        return None
+    return False
+
+
+def is_windows(current_platform: IdentifiedPlatformType) -> bool:
+    return current_platform == IdentifiedPlatformType.WINDOWS
+
+
+def is_linux(current_platform: IdentifiedPlatformType) -> bool:
+    return current_platform == IdentifiedPlatformType.LINUX
+
+
+def is_freebsd(current_platform: IdentifiedPlatformType) -> bool:
+    return current_platform == IdentifiedPlatformType.FREEBSD
+
+
+def is_macos(current_platform: IdentifiedPlatformType) -> bool:
+    return current_platform == IdentifiedPlatformType.MACOS
+
+
+def is_bsd(current_platform: IdentifiedPlatformType) -> bool:
+    return is_freebsd(current_platform)
+
+
+def is_unknown(current_platform: IdentifiedPlatformType) -> bool:
+    return current_platform == IdentifiedPlatformType.UNKNOWN
+
+
+def is_unix_like_foss(current_platform: IdentifiedPlatformType) -> bool:
+    return is_bsd(current_platform) or is_linux(current_platform)
+
+
+PLATFORM_INFO_FUNCS: dict[PlatformType, PlatformFunc] = {
+    PlatformType.WINDOWS: is_windows,
+    PlatformType.LINUX: is_linux,
+    PlatformType.MACOS: is_macos,
+    PlatformType.FREEBSD: is_freebsd,
+    PlatformType.UNKNOWN: is_unknown,
+    PlatformType.BSD: is_bsd,
+    PlatformType.ANY: lambda _: True,
+    PlatformType.UNIX_LIKE_FOSS: is_unix_like_foss,
+}

src/wakepy/methods/_testing.py
@@ -2,7 +2,7 @@
 activation success. It is controlled with the WAKEPY_FAKE_SUCCESS environment
 variable and meant to be used in CI pipelines / tests."""
 
-from wakepy.core import CURRENT_PLATFORM, Method
+from wakepy.core import Method, PlatformType
 from wakepy.core.constants import WAKEPY_FAKE_SUCCESS
 
 
@@ -16,7 +16,7 @@ class WakepyFakeSuccess(Method):
 
     name = WAKEPY_FAKE_SUCCESS
     mode_name = "_fake"
-    supported_platforms = (CURRENT_PLATFORM,)
+    supported_platforms = (PlatformType.ANY,)
 
     def enter_mode(self) -> None:
         """Does nothing ("succeeds" automatically; Will never raise an

commit 1661db2252dc3047b4eeafa768d9917f71590ec7

Fix prioritized order of Methods (#429)
Fixes: #428 

Description of the fixed bug: Sometimes wakepy tried different Methods
in the wrong order. For example, it could try the "caffeinate" method
(works only on MacOS) on Linux before any Linux-specific method. This
problem was introduced in https://github.com/fohrloop/wakepy/pull/379/
and was present in wakepy versions 0.10.0 to 0.10.2.

src/wakepy/core/prioritization.py
@@ -14,7 +14,7 @@ import typing
 from typing import List, Sequence, Set, Union
 
 from .constants import WAKEPY_FAKE_SUCCESS
-from .platform import CURRENT_PLATFORM
+from .platform import CURRENT_PLATFORM, get_platform_supported
 
 if typing.TYPE_CHECKING:
     from typing import List, Optional, Tuple, Union
@@ -254,7 +254,7 @@ def _order_set_of_methods_by_priority(methods: Set[MethodCls]) -> List[MethodCls
             0 if m.name == WAKEPY_FAKE_SUCCESS else 1,
             # Then, prioritize methods supporting CURRENT_PLATFORM over any
             # others
-            0 if CURRENT_PLATFORM in m.supported_platforms else 1,
+            0 if get_platform_supported(CURRENT_PLATFORM, m.supported_platforms) else 1,
             m.name.lower() if m.name else "",
         ),
     )

tests/unit/test_core/conftest.py
@@ -44,11 +44,11 @@ def provide_methods_different_platforms(monkeypatch, testutils):
 
     class LinuxB(TestMethod):
         name = "LinuxB"
-        supported_platforms = (PlatformType.LINUX,)
+        supported_platforms = (PlatformType.UNIX_LIKE_FOSS,)
 
     class LinuxC(TestMethod):
         name = "LinuxC"
-        supported_platforms = (PlatformType.LINUX,)
+        supported_platforms = (PlatformType.UNIX_LIKE_FOSS,)
 
     class MultiPlatformA(TestMethod):
         name = "multiA"

tests/unit/test_core/test_prioritization.py
@@ -5,7 +5,7 @@ import typing
 
 import pytest
 
-from wakepy.core import PlatformType
+from wakepy.core import IdentifiedPlatformType
 from wakepy.core.constants import WAKEPY_FAKE_SUCCESS
 from wakepy.core.prioritization import (
     _check_methods_priority,
@@ -14,6 +14,7 @@ from wakepy.core.prioritization import (
     order_methods_by_priority,
 )
 from wakepy.core.registry import get_methods
+from wakepy.methods import gnome, macos, windows
 
 if typing.TYPE_CHECKING:
     from typing import List, Type
@@ -25,7 +26,7 @@ if typing.TYPE_CHECKING:
 def set_current_platform_to_linux(monkeypatch):
 
     monkeypatch.setattr(
-        "wakepy.core.prioritization.CURRENT_PLATFORM", PlatformType.LINUX
+        "wakepy.core.prioritization.CURRENT_PLATFORM", IdentifiedPlatformType.LINUX
     )
 
 
@@ -34,7 +35,7 @@ def set_current_platform_to_windows(monkeypatch):
 
     monkeypatch.setattr(
         "wakepy.core.prioritization.CURRENT_PLATFORM",
-        PlatformType.WINDOWS,
+        IdentifiedPlatformType.WINDOWS,
     )
 
 
@@ -396,3 +397,18 @@ class TestOrderSetOfMethodsByPriority:
         assert _order_set_of_methods_by_priority(
             {WindowsA, WindowsB, WindowsC, LinuxA, LinuxB, LinuxC, MultiPlatformA}
         ) == [MultiPlatformA, WindowsA, WindowsB, WindowsC, LinuxA, LinuxB, LinuxC]
+
+    @pytest.mark.usefixtures("set_current_platform_to_linux")
+    def test_on_linux_2(self):
+        # This has failed previously.
+        # See: https://github.com/fohrloop/wakepy/issues/428
+
+        ordered = _order_set_of_methods_by_priority(
+            {
+                gnome.GnomeSessionManagerNoIdle,
+                macos.CaffeinateKeepPresenting,
+                windows.WindowsKeepPresenting,
+            }
+        )
+
+        assert ordered[0] == gnome.GnomeSessionManagerNoIdle