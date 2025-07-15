294cd1ec499142b0a67ddffc235e032a14da9305
Niko Föhr <17479683+fohrloop@users.noreply.github.com>
2025-07-15 18:13:55 +0300

    add: example-test-with-wakepy.py script (#451)
    This script is similar to scripts\example-test.py but there's a wakepy
    mode activated & deactivated before the timer starts. Can be used to
    determine the status of the system sleep timer right after exiting
    wakepy modes. (for #369 and #450)

---
+++ scripts/example-test-with-wakepy.py
@@ -0,0 +1,29 @@
+"""This is a simple script for testing wakepy (manually). It is similar to
+scripts\example-test.py but there's a wakepy mode activated & deactivated
+before the timer starts.
+
+Can be used to determine the status of the system sleep timer right after
+exiting the wakepy mode.
+"""
+
+import datetime as dt
+import time
+
+from wakepy import keep
+
+start = dt.datetime.now()
+now = None
+
+print(dt.datetime.now().strftime("%b %d %H:%M:%S"))
+print("Wakepy inhibit start")
+with keep.presenting():
+    time.sleep(6 * 60)
+    print("Wakepy inhibit end")
+
+while True:
+    prev = now or dt.datetime.now()
+    now = dt.datetime.now()
+    now_str = now.strftime("%b %d %H:%M:%S")
+    delta_str = f"{(now-prev)/dt.timedelta(seconds=1)}s"
+    print(f"{now_str} | elapsed {now-start} | delta: {delta_str}")
+    time.sleep(2)
