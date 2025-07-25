b62d4136b4829409c75f18b6582b2b4524c4becc
Niko Föhr <17479683+fohrloop@users.noreply.github.com>
2025-07-26 00:34:52 +0300

    solution using explicit .close()

--- src/wakepy/methods/macos.py
+++ src/wakepy/methods/macos.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
 import logging
+import typing
 from abc import ABC, abstractmethod
-from contextlib import ExitStack
+from io import IOBase
 from subprocess import PIPE, Popen
+from typing import cast
 
 from wakepy.core import Method, ModeName, PlatformType
+
+if typing.TYPE_CHECKING:
+    from typing import Optional
 
 
 class _MacCaffeinate(Method, ABC):
@@ -20,23 +25,32 @@ class _MacCaffeinate(Method, ABC):
     def __init__(self, **kwargs: object) -> None:
         super().__init__(**kwargs)
         self.logger = logging.getLogger(__name__)
-        self._exit_stack: ExitStack | None = None
+        self._process: Optional[Popen[bytes]] = None
 
     def enter_mode(self) -> None:
         self.logger.debug('Running "%s"', self.command)
-        self._exit_stack = ExitStack()
-        self._exit_stack.enter_context(
-            Popen(self.command.split(), stdin=PIPE, stdout=PIPE)
-        )
+        self._process = Popen(self.command.split(), stdin=PIPE, stdout=PIPE)
 
     def exit_mode(self) -> None:
-        if self._exit_stack is None:
+        if self._process is None:
             self.logger.debug("No need to terminate process (not started)")
             return
         self.logger.debug('Terminating process ("%s")', self.command)
-        self._exit_stack.close()
-        self._exit_stack = None
-        self.logger.debug("Process terminated")
+
+        # The pipes need to be closed before terminating the process, otherwise
+        # will get ResourceWarning: unclosed file
+        # See: https://stackoverflow.com/a/58696973/3015186 and
+        # https://github.com/fohrloop/wakepy/issues/478
+        #
+        # The cast is required because we know that these are not None, but
+        # mypy doesn't, and using if statements would ruin test coverage.
+        stdin = cast(IOBase, self._process.stdin)
+        stdout = cast(IOBase, self._process.stdout)
+        stdin.close()
+        stdout.close()
+
+        self._process.terminate()
+        self._process.wait()
 
     @property
     @abstractmethod
